f3f2af776e12614e7b9425a73024a9bd8a5fbd61
Max <maxlu0428@gmail.com>
2024-05-22 01:23:57 +0800

    add Bootstrap css

--- smartnotes/notes/urls.py
+++ smartnotes/notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list  ),
+    path('notes', views.list ,name='note.list'),
     path('notes/<int:pk>',views.detail)
 
 ]
